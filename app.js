0c66b7b84e51bef58589269c6ebf521a408e982c
Navnit Tiwari <42030960+navnittiwari@users.noreply.github.com>
2020-07-05 17:25:58 +0530

    till challenge 13

--- app.js
+++ app.js
@@ -9,46 +9,44 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home", {StartingContent: homeStartingContent});
+app.get("/", function (req, res) {
+  res.render("home", {
+    StartingContent: homeStartingContent,
+    posts: posts
+  });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutPageContent: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutPageContent: aboutContent });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactPageContent: contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactPageContent: contactContent });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 })
 
-app.post("/compose", function(req, res){
+app.post("/compose", function (req, res) {
   const newPost = {
-    title:req.body.postTitle,
-    content:req.body.postBody
+    title: req.body.postTitle,
+    content: req.body.postBody
   }
+  posts.push(newPost);
+  res.redirect("/");
 })
 
 
 
 
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
